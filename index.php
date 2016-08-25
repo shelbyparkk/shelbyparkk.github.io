93fc708ba4c8977b1a3fe9a9876c740bf5708104
shelbyparkk <jp829@cornell.edu>
2016-08-24 23:55:44 -0700

    add

---
+++ index.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html>
+<body>
+      <div class = "header_container">
+        <h1>spark</h1>
+        <div class = "line"></div> <!--line -->
+        <p>the blurb</p>
+      </div>
+    </div> <!-- title&header end -->
+  </section>
+
+    <div class = "nav_tabs">
+      <div class = "nav_tabcontainer">
+        <div class = "tab">
+          <a class="scroll" href="#about">
+          <div class = "circle"></div>
+          <p>ABOUT</p>
+          </a>
+        </div>
+        <div class = "tab">
+          <a class="scroll" href="#study">
+          <div class = "circle"></div>
+          <p>AREAS OF STUDY</p>
+          </a>
+        </div>
+        <div class = "tab">
+          <a class="scroll" href="#edu">
+          <div class = "circle"></div>
+          <p>EDUCATION & LINKS</p>
+          </a>
+        </div>
+        <div class = "tab">
+          <a class="scroll" href="#works">
+          <div class = "circle"></div>
+          <p>WORK SAMPLES</p>
+          </a>
+        </div>
+        <div class = "tab">
+          <a class="scroll" href="#contact">
+          <div class = "circle"></div>
+          <p>CONTACT</p>
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <section id = "about">
+    <div class = "about">
+      <div class = "about_container">
+        <h2> ABOUT ME </h2>
+        <div class = "line"></div>
+        <p class = "body_p"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+      </div>
+    </div>
+    </section>
+
+    <section id = "study">
+    <div class = "study">
+      <div class = "study_container">
+        <h2> AREAS OF STUDY </h2>
+        <div class = "line"></div>
+        <div class = "study_module" id="left">
+          <div class = "study_module_picture">
+            <img src="images/dev.png" alt="Traditional"/>
+          </div>
+          <div class = "study_module_bar">
+            <h3>TRADITIONAL</h3>
+            <p>Watercolor, acrylics, graphite</p>
+          </div>
+        </div>
+        <div class = "study_module" id="middle">
+          <div class = "study_module_picture">
+            <img src="images/illu.png" alt="Digital"/>
+          </div>
+          <div class = "study_module_bar">
+            <h3>DIGITAL</h3>
+            <p>Logos, Illustrator, Photoshop</p>
+          </div>
+        </div>
+        <div class = "study_module" id="right">
+          <div class = "study_module_picture">
+            <img src="images/des.png" alt="UIUX"/>
+          </div>
+          <div class = "study_module_bar">
+            <h3>UX/UI DESIGN</h3>
+            <p>From workflow diagrams to prototypes</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id = "edu">
+    <div class = "edu">
+      <div class = "edu_header_container">
+        <h2> EDUCATION </h2>
+        <div class = "line"></div>
+      </div>
+      <div class = "edu_container">
+
+        <div class = "edu_margin">        
+          <div class = "edu_explanation">
+          <h2> Cornell University Undergrad </h2>
+          <p> a waste of space </p>
+        </div>
+          <div class ="image"></div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id = "works">
+    <div class = "works">
+      <div class = "works_container">
+        <h2>WORK SAMPLES</h2>
+        <div class = "line"></div>
+        <a href ="projecta.php"> <div class = "worksthumbnail" id="a"></div> </a>
+        <a href ="projectb.php"> <div class = "worksthumbnail" id="b"></div></a>
+        <a href ="projectc.php"> <div class = "worksthumbnail" id="c"></div> </a>
+        <a href ="projectd.php"><div class = "worksthumbnail" id="d"></div></a>
+        <a href ="projecte.php"><div class = "worksthumbnail" id="e"></div></a>
+        <a href ="projectf.php"><div class = "worksthumbnail" id="f"></div></a>
+        <a href ="projectg.php"><div class = "worksthumbnail" id="g"></div></a>
+        <a href ="projecth.php"><div class = "worksthumbnail" id="h"></div></a>
+      </div>
+    </div>
+  </section>
+
+  <section id = "contact">
+    <div class = "contact">
+      <div class = "contact_container">
+        <h2> PLEASE GIVE ME A JOB </h2>
+        <div class="line" id="body"></div>
+        <div class = "contact_button">
+          <a href="mailto:winterbyfall@gmail.com">
+          <p id="body_p"> CONTACT </p>
+          </a>
+        </div>
+      </div>
+    </div> <!--contact end -->
+  </section>
+    <div class ="line_full"></div>
+    
+</body>
+</html>
